32a22b28ac46515f6a19ae9f8a737e4c426e8954
ArtRayd <artrayd@gmail.com>
2020-03-17 18:09:00 +0200

    Compiled files

---
+++ dist/precache-manifest.cb2c2808a2f4e3ce7baa9d5655fb7184.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd76220ccfb72898d5d",
+    "url": "/css/app.741d516e.css"
+  },
+  {
+    "revision": "7c7ef8992b6f738f1c50",
+    "url": "/css/chunk-vendors.a69551cc.css"
+  },
+  {
+    "revision": "89f5c89a99ca06b5fd3e16aac02a62c3",
+    "url": "/img/ava.gif"
+  },
+  {
+    "revision": "6ac94677985ef3f121960f312796f957",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "bab5a9e449d6dc0b15b70a2f3829d126",
+    "url": "/img/photo.jpg"
+  },
+  {
+    "revision": "116ec78e01c72387f5952f4b464c76a7",
+    "url": "/img/spa.svg"
+  },
+  {
+    "revision": "4657880d87607d6d78b7dcda8c714794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd76220ccfb72898d5d",
+    "url": "/js/app.7f28cdb2.js"
+  },
+  {
+    "revision": "7c7ef8992b6f738f1c50",
+    "url": "/js/chunk-vendors.5bcb520c.js"
+  },
+  {
+    "revision": "64f85719d2b0ef551f3a2b6fd5e0de10",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "178d8faa674e2741bbf5d8f879ae18ef",
+    "url": "/posts.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
